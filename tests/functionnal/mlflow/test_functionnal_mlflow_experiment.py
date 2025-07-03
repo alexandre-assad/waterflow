bd47c17888abcbe1e3e180d1fd281200b4a66cdd
Clémentine TRUCCHI <alexandre.assad@laplateforme.io>
2025-07-03 11:01:47 +0200

    :fix: use tmp path in experiment

--- tests/functionnal/mlflow/test_functionnal_mlflow_experiment.py
+++ tests/functionnal/mlflow/test_functionnal_mlflow_experiment.py
@@ -1,10 +1,19 @@
 import mlflow
+from pathlib import Path
+
 from mlflow.tracking import MlflowClient
 from waterflow.experiment import preprocess_data, create_model, create_model_tuned
 
+def test_mlflow_tracking(tmp_path):
+    mlflow.set_tracking_uri(Path(tmp_path).absolute().as_uri())
+    mlflow.set_experiment("test_exp")
+    with mlflow.start_run():
+        mlflow.log_param("param1", 5)
+        mlflow.log_metric("metric1", 0.9)
+
 def test_full_pipeline_logging(tmp_path):
     mlflow.set_experiment("test_experiment")
-    X_train, y_train, X_test, y_test = preprocess_data()
+    X_train, y_train, X_test, y_test = preprocess_data(registry=False)
 
     with mlflow.start_run(run_name="XGBoost"):
         create_model(X_train, y_train, X_test, y_test)
